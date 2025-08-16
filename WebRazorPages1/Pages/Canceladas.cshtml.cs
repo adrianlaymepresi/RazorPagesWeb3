e12978838b589f724bd819709e6d454ece95e6c2
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-08-15 21:18:45 -0400

    AdrianLayme/15Agosto/CreacionInterfaz

---
+++ WebRazorPages1/Pages/Canceladas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorPages1.Pages
+{
+    public class CanceladasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
